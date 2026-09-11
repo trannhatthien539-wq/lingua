95575ec58ff235fc2afc1d22bce992658a0abb05
trannhatthien539-wq <trannhatthien539@gmail.com>
2026-09-11 18:32:46 +0100

    Fix Firebase config and deployment

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,24 +1,21 @@
-import { getApp, getApps, initializeApp } from 'firebase/app'
-import { browserLocalPersistence, getAuth, GoogleAuthProvider, setPersistence, signOut, onAuthStateChanged } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDbfWmStltXb9l8YC5ySE7jtffoqU1J3hBY',
-  authDomain: 'lingua-49fc1.firebaseapp.com',
-  projectId: 'lingua-49fc1',
-  storageBucket: 'lingua-49fc1.firebasestorage.app',
-  messagingSenderId: '138926081617',
-  appId: '1:138926081617:web:4d64b990f6dfc6c9e41a70',
-  measurementId: 'G-JG9KNJ4Y4Y',
-}
+  apiKey: "AIzaSyDbfWmStltxB9l8YC5ySE7jtffqU1J3hBY",
+  authDomain: "lingua-49fc1.firebaseapp.com",
+  projectId: "lingua-49fc1",
+  storageBucket: "lingua-49fc1.firebasestorage.app",
+  messagingSenderId: "138926081617",
+  appId: "1:138926081617:web:4d64b990f6dfc6c9e41a70",
+  measurementId: "G-JG9KNJ4Y4Y"
+};
 
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
-
-// Khởi tạo các dịch vụ
-export const auth = getAuth(app)
-export const googleProvider = new GoogleAuthProvider()
-export const db = getFirestore(app)
-export const authPersistence = setPersistence(auth, browserLocalPersistence)
-
-// Export các hàm tiện ích
-export { signOut, onAuthStateChanged }
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
